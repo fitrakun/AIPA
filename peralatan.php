ba1b46f15eb5373787a1bffbfc4b910034d8bbb3
fitrakun <fitrakun@gmail.com>
2016-02-01 18:17:10 +0700

    add navbar

---
+++ peralatan.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>AIPA</title>
+        <meta name="description" content="Using for PPL task">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">AIPA</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                      <li><a href="#">Peminjaman</a></li>
+                      <li><a href="#">Pengembalian</a></li>
+                      <li><a href="#">Booking</a></li>
+                      <li><a href="#">Perbaikan</a></li>
+                      <li class="active"><a href="peralatan.php">Peralatan</a></li>
+                      <li><a href="#">Statistik</a></li>
+                    </ul>
+                </div>
+            </nav>
+    </body>
+</html>
